543e56b4cb20c3d306cb14b033b2529804ee43ec
Hanz <22578473+fivehanz@users.noreply.github.com>
2022-09-03 16:25:49 +0000

    chore: solution 1.1.3

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,46 @@
-import type { NextPage } from 'next'
-import { useState } from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import AddressForm from '../components/AddressForm'
+import type { NextPage } from "next";
+import { useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import AddressForm from "../components/AddressForm";
+import * as Web3 from "@solana/web3.js";
 
 const Home: NextPage = () => {
-  const [balance, setBalance] = useState(0)
-  const [address, setAddress] = useState('')
+  const [balance, setBalance] = useState(0);
+  const [address, setAddress] = useState("");
+  const [isExecutable, setIsExecutable] = useState(false);
 
-  const addressSubmittedHandler = (address: string) => {
-    setAddress(address)
-    setBalance(1000)
-  }
+  const addressSubmittedHandler = async (address: string) => {
+    try {
+      const key = new Web3.PublicKey(address);
+      setAddress(key.toBase58());
+
+      const connection = new Web3.Connection(Web3.clusterApiUrl("devnet"));
+      connection
+        .getBalance(key)
+        .then((balance) => setBalance(balance / Web3.LAMPORTS_PER_SOL));
+
+      const info = await connection.getAccountInfo(key);
+      setIsExecutable(info!.executable);
+    } catch (error) {
+      setAddress("");
+      setBalance(0);
+      alert(error);
+    }
+  };
 
   return (
     <div className={styles.App}>
       <header className={styles.AppHeader}>
-        <p>
-          Start Your Solana Journey
-        </p>
+        <p>Start Your Solana Journey</p>
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Is it executable? ${isExecutable ? "Yes" : "Nope"}`}</p>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
